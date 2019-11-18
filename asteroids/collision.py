cbcddcef7d2b3da183f7bd7a693d2c4b05dcce95
JaakkoKaikkonen <42070829+JaakkoKaikkonen@users.noreply.github.com>
2019-11-18 23:41:31 +0200

    Delete collision.py

--- asteroids/collision.py
+++
@@ -1,7 +0,0 @@
-def distanceSquared(x1, y1, x2, y2):
-    deltaX = x2 - x1
-    deltaY = y2 - y1
-    return deltaX * deltaX + deltaY * deltaY
-
-def circleCollision(x1, y1, radius1, x2, y2, radius2):
-    return distanceSquared(x1, y1, x2, y2) < ((radius1 + radius2) * (radius1 + radius2))
